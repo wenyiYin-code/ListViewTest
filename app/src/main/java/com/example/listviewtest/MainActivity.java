41325db0ac5edd967691d0a81ff96a9cbf9bfb6b
wenyiYin <443644234@qq.com>
2023-10-13 15:52:09 +0800

    4.新增ListView子项点击相应事件逻辑

--- app/src/main/java/com/example/listviewtest/MainActivity.java
+++ app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
                 R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initFruits(){
