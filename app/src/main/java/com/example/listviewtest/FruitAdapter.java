7c77f7babc28ceaafb46ef5600f21c3750165cfd
wenyiYin <443644234@qq.com>
2023-10-13 15:39:13 +0800

    3.优化适配器显示view逻辑，提高效率

--- app/src/main/java/com/example/listviewtest/FruitAdapter.java
+++ app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 public class FruitAdapter extends ArrayAdapter<Fruit> {
@@ -21,13 +23,27 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Fruit fruit = getItem(position); // 获取当前项的Fruit实例
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent,
-                false);
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent,
+                    false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById (R.id.fruit_name);
+            view.setTag(viewHolder); // 将ViewHolder存储在View中
+        }else{
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag(); // 重新获取ViewHolder
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
     }
 
 }
